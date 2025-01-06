55e7a50b67c80ec49dfc78a0d97340d3d902a1d4
uu-z <ncysatnaf@gmail.com>
2025-01-06 15:17:59 +0800

    fix api

--- server.ts
+++ server.ts
@@ -2,16 +2,17 @@ import { BinoAI } from './src/binoai';
 import { Hono } from 'hono'
 const app = new Hono()
 
-const binoai = new BinoAI();
 
 app.get("/", (c) => {
     return c.text("hello world!")
 })
 
 app.post('/ask', async (c) => {
+    const binoai = new BinoAI();
+
     const APIKEY = c.req.header("API-KEY")
     // TODO handle API
-    const content = c.req.param("content") && (await c.req.json()).content
+    const content = c.req.query("content") || (await c.req.json()).content
     const response = await binoai.agent.run(content);
     return c.json({ data: response })
 })
