6608a344756bc83a92830a170a0c05cc84e92e0c
osdodo <dodomatrix@pm.me>
2025-01-10 13:46:41 +0800

    minor fixes

--- src/SentientAI.ts
+++ src/SentientAI.ts
@@ -5,18 +5,29 @@ import {
   ForecastWeatherAPITool,
 } from "./tools/weatherapi"; // Go up one level, then into src/tools
 
+const { OPENAI_API_KEY, NUBILA_API_KEY, OPENWEATHER_API_KEY, NEWSAPI_API_KEY } =
+  process.env;
+if (
+  !OPENAI_API_KEY ||
+  !NUBILA_API_KEY ||
+  !OPENWEATHER_API_KEY ||
+  !NEWSAPI_API_KEY
+) {
+  throw new Error("Missing environment variables");
+}
+
 export class SentientAI {
   weatherAgent = new Agent({
     name: "Weather Agent",
     description:
       "Get current weather with CurrentWeatherAPITool and forecast weather with ForecastWeatherAPITool.",
     tools: [
-      new CurrentWeatherAPITool(process.env.NUBILA_API_KEY!),
-      new ForecastWeatherAPITool(process.env.OPENWEATHER_API_KEY!),
+      new CurrentWeatherAPITool(NUBILA_API_KEY!),
+      new ForecastWeatherAPITool(OPENWEATHER_API_KEY!),
     ],
   });
 
-  newsTool = new NewsAPITool(process.env.NEWSAPI_API_KEY!);
+  newsTool = new NewsAPITool(NEWSAPI_API_KEY!);
 
   agent = new Agent({
     tools: [this.weatherAgent, this.newsTool],
