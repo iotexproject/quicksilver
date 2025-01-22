2e58d19377400cab24568570dde990717327619e
Nikita Ruban <nikitkaruban@gmail.com>
2025-01-22 14:02:55 +0000

    chore: Update vitest configuration to include additional test directories and coverage files

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,11 +4,11 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     setupFiles: ["./vitest.setup.ts"],
-    include: ["src/**/*.test.ts"],
+    include: ["src/**/*.test.ts", "tests/**/*.test.ts"],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
-      include: ['src/**/*.ts'],
+      include: ['src/**/*.ts', "server.ts"],
       exclude: ['src/**/*.test.ts', 'src/**/mocks.ts']
     }
   },
