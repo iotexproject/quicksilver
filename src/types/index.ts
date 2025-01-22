f6fa067f43bb85da5bd7819f7daa8c4d07c3a4ed
Nikita Ruban <nikitkaruban@gmail.com>
2025-01-22 16:39:26 +0000

    refactor: Update PromptContext to support multiple tools and outputs

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,6 @@
 export interface PromptContext {
-  tool: Tool;
-  toolOutput: string;
-  toolInput: string;
+  tools: Tool[];
+  toolOutputs: string[];
   input: string;
 }
 
@@ -9,16 +8,5 @@ export interface Tool {
   name: string;
   description: string;
   twitterAccount?: string;
-  execute(input: string): Promise<string>;
-}
-
-export interface ActionResult {
-  tool?: Tool;
-  output: string;
-}
-
-export interface IAgent {
-  name: string;
-  description: string;
   execute(input: string): Promise<string>;
 }
