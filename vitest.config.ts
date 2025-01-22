907b7289ebd79daff153a29b84f6625d6db6538c
Nikita Ruban <nikitkaruban@gmail.com>
2025-01-22 12:10:38 +0000

    chore: Update .gitignore and vitest configuration to include coverage settings and additional test files

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    setupFiles: ["./vitest.setup.ts"], // Specify the setup file
+    setupFiles: ["./vitest.setup.ts"],
+    include: ["src/**/*.test.ts"],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*.ts'],
+      exclude: ['src/**/*.test.ts', 'src/**/mocks.ts']
+    }
   },
 });
