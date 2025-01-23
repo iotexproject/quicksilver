1e92bfde4751bd4508abb83f8a80b2ebe65ce5c2
Nikita Ruban <nikitkaruban@gmail.com>
2025-01-23 11:52:49 +0000

    feat: Add extractContentFromTags utility and corresponding tests

---
+++ src/utils/parsers.ts
@@ -0,0 +1,5 @@
+export const extractContentFromTags = (content: string, tag: string) => {
+  const regex = new RegExp(`<${tag}>(.*?)<\/${tag}>`, "s");
+  const match = content.match(regex);
+  return match ? match[1] : null;
+};
